679135f8b4ac23076203ed33b2e8bf5c0adb5df6
kastorcode <ramalholiveira13@gmail.com>
2022-09-20 09:39:17 -0300

    Follow and unfollow users on SearchFragment Add firebaseUser, followingRef props, checkFollowingStatus, followUser and unfollowUser functions in UserAdapter

--- app/src/main/java/com/kastorcode/instagramclone/Adapters/UserAdapter.kt
+++ app/src/main/java/com/kastorcode/instagramclone/Adapters/UserAdapter.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import com.kastorcode.instagramclone.Models.User
 import com.kastorcode.instagramclone.R
 import com.squareup.picasso.Picasso
@@ -19,6 +22,13 @@ class UserAdapter (
     private val isFragment : Boolean = false
 ) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
+    private var firebaseUser : FirebaseUser? = FirebaseAuth.getInstance().currentUser
+    private val followingRef : DatabaseReference = firebaseUser?.uid.let {
+        FirebaseDatabase.getInstance().reference.child("Follow")
+            .child(it.toString()).child("Following")
+    }
+
+
     override fun onCreateViewHolder (parent : ViewGroup, viewType: Int) : UserAdapter.ViewHolder {
         val view = LayoutInflater.from(mContext)
             .inflate(R.layout.user_item_layout, parent, false)
@@ -27,12 +37,79 @@ class UserAdapter (
 
 
     override fun onBindViewHolder (holder : UserAdapter.ViewHolder, position : Int) {
+        fun checkFollowingStatus (uid : String, followButton : Button) {
+            followingRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange (snapshot : DataSnapshot) {
+                    if (snapshot.child(uid).exists()) {
+                        followButton.text = "Following"
+                    }
+                    else {
+                        followButton.text = "Follow"
+                    }
+                }
+
+                override fun onCancelled (error : DatabaseError) {
+                }
+            })
+        }
+        fun followUser (user : User) {
+            firebaseUser?.uid.let { it ->
+                followingRef.child(user.getUid()).setValue(true)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            firebaseUser?.uid.let { it ->
+                                FirebaseDatabase.getInstance().reference.child("Follow")
+                                    .child(user.getUid()).child("Followers")
+                                    .child(it.toString()).setValue(true)
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                        }
+                                        else {
+                                        }
+                                    }
+                            }
+                        }
+                        else {
+                        }
+                    }
+            }
+        }
+        fun unfollowUser (user : User) {
+            firebaseUser?.uid.let { it ->
+                followingRef.child(user.getUid()).removeValue()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            firebaseUser?.uid.let { it ->
+                                FirebaseDatabase.getInstance().reference.child("Follow")
+                                    .child(user.getUid()).child("Followers")
+                                    .child(it.toString()).removeValue()
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                        }
+                                        else {
+                                        }
+                                    }
+                            }
+                        }
+                        else {
+                        }
+                    }
+            }
+        }
         val user = mUser[position]
         Picasso.get().load(user.getImage())
             .placeholder(R.drawable.profile).into(holder.imageImageView)
         holder.userNameTextView.text = user.getUserName()
         holder.fullNameTextView.text = user.getFullName()
-        holder.followButton
+        checkFollowingStatus(user.getUid(), holder.followButton)
+        holder.followButton.setOnClickListener {
+            if (holder.followButton.text.toString() == "Follow") {
+                followUser(user)
+            }
+            else {
+                unfollowUser(user)
+            }
+        }
     }
 
 
